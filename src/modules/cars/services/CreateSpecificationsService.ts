c36294f956bb168bbedb15ab7051c0658fe0588a
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-07-21 00:13:48 -0300

    feat: specification service created and verified

--- src/modules/cars/services/CreateSpecificationsService.ts
+++ src/modules/cars/services/CreateSpecificationsService.ts
@@ -6,8 +6,13 @@ interface IRequest {
 }
 class CreateSpecificationsService {
   constructor(private specificationRepository: ISpecificationsRepository) { }
-
   execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists = this.specificationRepository.findByName(name)
+
+    if (specificationAlreadyExists) {
+      throw new Error('Specification already exists!')
+    }
+
     this.specificationRepository.create({
       name,
       description
