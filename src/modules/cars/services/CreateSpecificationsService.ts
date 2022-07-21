0ba14669854b2a99a2e47fd7ac639f7d1f85d467
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-07-21 00:06:08 -0300

    feat: specification service class

--- src/modules/cars/services/CreateSpecificationsService.ts
+++ src/modules/cars/services/CreateSpecificationsService.ts
@@ -1,6 +1,17 @@
-class CreateSpecificationsService {
-  execute() {
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
 
+interface IRequest {
+  name: string
+  description: string
+}
+class CreateSpecificationsService {
+  constructor(private specificationRepository: ISpecificationsRepository) { }
+
+  execute({ name, description }: IRequest): void {
+    this.specificationRepository.create({
+      name,
+      description
+    })
   }
 }
 
