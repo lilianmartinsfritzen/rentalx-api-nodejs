79366d2b7bc3a80a18c7c858041ccb2be22884c6
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-07-23 01:57:44 -0300

    docs: implementation of api documentation started

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from './swagger.json'
+
 import { router } from './routes'
+import swaggerFile from './swagger.json'
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(router)
 
