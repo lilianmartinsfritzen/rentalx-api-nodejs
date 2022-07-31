b40bc414c28a566a1fa47fa7ef105aa9fb58bc4e
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-07-31 18:46:19 -0300

    feat: docker and orm config

---
+++ ormconfig.js
@@ -0,0 +1,8 @@
+module.exports ={
+  "type": "postgres",
+  "port": process.env.ORM_PORT,
+  "host": process.env.ORM_HOST,
+  "username": process.env.ORM_USERNAME,
+  "password": process.env.ORM_PASSWORD,
+  "database": "rentx"
+}
