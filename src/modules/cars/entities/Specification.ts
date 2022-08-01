9753c629b6b023b29c1808e645e4952fc81e5ec6
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-08-01 00:33:59 -0300

    refactor: migration to specification created

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,9 +1,18 @@
-import { v4 } from "uuid"
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
+import { v4 } from 'uuid'
 
+@Entity('specifications')
 class Specification {
+  @PrimaryColumn()
   id?: string
+
+  @Column()
   name: string
+
+  @Column()
   description: string
+
+  @CreateDateColumn()
   created_at: Date
 
   constructor() {
