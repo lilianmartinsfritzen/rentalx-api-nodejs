b1ed346565602bd2c35805cf0d0b2aed8c0a139e
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-07-31 23:34:50 -0300

    refactor: for tsyringe implementation

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,12 @@ import 'dotenv/config'
 import swaggerUi from 'swagger-ui-express'
 import swaggerDocument from './swagger.json'
 
+import './database'
+
+import './shared/container'
+
 import { router } from './routes'
 import swaggerFile from './swagger.json'
-
-import "./database"
 
 const app = express()
 
