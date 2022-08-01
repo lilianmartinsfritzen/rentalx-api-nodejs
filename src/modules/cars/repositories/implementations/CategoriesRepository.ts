9fe6f39f081fc3d34a013eea1aa47517cff81c28
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-07-31 21:17:01 -0300

    refactor: create method from categories repository

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,13 +1,14 @@
+import { getRepository, Repository } from "typeorm"
 import { Category } from "../../entities/Category"
 import { ICategoriesRepository, ICreateCategoryDTO } from "../ICategoriesRepository"
 
 class CategoriesRepository implements ICategoriesRepository {
-  private categories: Category[]
 
+  private repository: Repository<Category>
   private static INSTANCE: CategoriesRepository
 
   private constructor() {
-    this.categories = []
+    this.repository = getRepository(Category)
   }
 
   public static getInstance(): CategoriesRepository {
@@ -17,16 +18,13 @@ class CategoriesRepository implements ICategoriesRepository {
     return CategoriesRepository.INSTANCE
   }
 
-  create({ name, description }: ICreateCategoryDTO) {
-    const category = new Category()
-
-    Object.assign(category, {
-      name,
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const category = this.repository.create({
       description,
-      created_at: new Date()
+      name,
     })
 
-    this.categories.push(category)
+    await this.repository.save(category)
   }
 
   list(): Category[] {
