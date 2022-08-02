c5d5fce564243c733bb4e468c9021e6bdf83d9cc
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-08-01 23:07:59 -0300

    feat: users use case and container users repository created

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,18 +1,25 @@
-import { container } from "tsyringe";
+import { container } from 'tsyringe';
 
-import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
-import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
+import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository';
+import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
 
-import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
-import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
+import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
+import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
 
+import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
+import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
 
 container.registerSingleton<ICategoriesRepository>(
-  "CategoriesRepository",
+  'CategoriesRepository',
   CategoriesRepository
 )
 
 container.registerSingleton<ISpecificationsRepository>(
-  "SpecificationsRepository",
+  'SpecificationsRepository',
   SpecificationsRepository
+)
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository
 )
