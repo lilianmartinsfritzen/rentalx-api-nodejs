9b668c1f8b0aafc06b72ec7089fe95bd352ac686
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-08-04 22:18:56 -0300

    test: category creation

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,30 @@
+import { AppError } from '../../../../errors/AppError'
+import { CategoriesRepositoryInMemory } from '../../repositories/in-memory/CategoriesRepositoryInMemory'
+import { CreateCategoryUseCase } from './CreateCategoryUseCase'
+
+let createCategoryUseCase: CreateCategoryUseCase
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory
+
+describe('Create Category', () => {
+
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory()
+    createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryInMemory)
+  })
+
+  it('should be able to be create a new category', async () => {
+    const category = {
+      name: 'Category Test',
+      description: 'Category description Test'
+    }
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description
+    })
+
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(category.name)
+
+    expect(categoryCreated).toHaveProperty('id')
+  })
+
+})
