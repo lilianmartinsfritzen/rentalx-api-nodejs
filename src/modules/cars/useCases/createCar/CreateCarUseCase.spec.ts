e23c38186f4921c7f5d75ae5b25aeac6b59ea946
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-08-12 23:50:52 -0300

    test: validation license plate

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from '../../../../shared/errors/AppError'
 import { CarsRepositoryInMemory } from '../../repositories/in-memory/CarsRepositoryInMemory'
 import { CreateCarUseCase } from './CreateCarUseCase'
 
@@ -9,6 +10,7 @@ describe('Create Car', () => {
     carsRepositoryInMemory = new CarsRepositoryInMemory()
     createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory)
   })
+
   it('should be able to create a new car', async () => {
     await createCarUseCase.execute({
       name: 'Name Car Test',
@@ -20,4 +22,29 @@ describe('Create Car', () => {
       category_id: 'category_id_test'
     })
   })
+
+  it('should be able to create a new car with exists license plate', async () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: 'Car1 Test',
+        description: 'Description Car Test',
+        daily_rate: 100,
+        license_plate: 'ABC-1234',
+        fine_amount: 60,
+        brand: 'Brand Test',
+        category_id: 'category_id_test'
+      })
+
+      await createCarUseCase.execute({
+        name: 'Car2 Test',
+        description: 'Description Car Test',
+        daily_rate: 100,
+        license_plate: 'ABC-1234',
+        fine_amount: 60,
+        brand: 'Brand Test',
+        category_id: 'category_id_test'
+      })
+    }).rejects.toBeInstanceOf(AppError)
+  })
+  
 })
